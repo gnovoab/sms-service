c7ec94dec08dc5a8cd81f416dcaf63c19306a7f3
gnovoab <Waze4Wo0!>
2020-09-01 23:58:55 +0100

    Change logging format

--- src/main/java/com/ecommerce/sms/repository/ProductStockRepository.java
+++ src/main/java/com/ecommerce/sms/repository/ProductStockRepository.java
@@ -5,6 +5,9 @@ package com.ecommerce.sms.repository;
 //Imports
 import com.ecommerce.sms.domain.model.ProductStock;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface ProductStockRepository extends CrudRepository<ProductStock, Long> {
 }
